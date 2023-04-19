cfcc91153fa6ed560dfe8e1a0edcd9333d48f5ae
Takshak Mudgal <114814639+takshakmudgal@users.noreply.github.com>
2023-04-19 12:01:22 +0530

    spelling error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import Weather from "./components/weather";
+import Weather from "./components/Weather";
 import Footer from "./components/Footer";
 function App() {
   const [] = useState(0);
